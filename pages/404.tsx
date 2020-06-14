fdf10998cc3def59ae86f71c8836814180c057af
Joshua Wiggins <joshua_kar@hotmail.com>
2020-06-14 00:29:28 -0400

    Added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,20 @@
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+
+import HtmlHead from "../HtmlHead";
+import Unkownpage from "../UnknownRoute/UnknownRoute";
+
+export default function Custom404() {
+  return (
+    <div className="custom404">
+      <HtmlHead
+        title="Not Found - ASDA Errand & Rental Service, Barbados"
+        contentDescription={
+          "Located in Barbados, ASDA Errand & Rental Services is dedicated to saving you time, stress and all at a valued price! Visit our main website..."
+        }
+      />
+      <Unkownpage />
+    </div>
+  );
+}
